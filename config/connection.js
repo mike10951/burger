a6070f20f8e588291bdd9d8be8bd04396b21833f
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2019-12-12 10:00:56 -0600

    Solved issues in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
